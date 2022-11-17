32b9b190a36a0c7e6a1296efbe059f0ced627d0a
kongtaoxing <18333318658@163.com>
2022-11-17 06:36:57 -0800

    Wait and see

--- contracts/xyzswapV2Factory.sol
+++ contracts/xyzswapV2Factory.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./xyzswapV2Pair.sol";
 import "./libraries/Math.sol";
 import "./libraries/SafeMath.sol";
 
@@ -17,13 +18,12 @@ interface Erc20Func {
     function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 }
 
-contract XyzswapV2 is ERC20 {
+contract XyzswapV2Factory is ERC20 {
 
     address owner;
     uint256 lpAmount;
     uint256 public fee;
-    address tokenA;
-    address tokenB;
+    address[] public allPairs;
 
     error notApproved();
     error notOwner();
@@ -32,19 +32,16 @@ contract XyzswapV2 is ERC20 {
     error invalidSigLen();
     error sigAlreadyUsed();
     error invalidAmount();
+    error zeroAddress();
 
-    //event for LP
-    event AddLiquid(address indexed tokenA, uint256 _amountA, address indexed tokenB, uint256 _amountB);
-    event RemoveLiquid(uint256 indexed _amount);
-    event Swap(address indexed _token, uint256 indexed _amount);
-
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
     // events for owner
     event ChangeOwner(address indexed newOwner);
 
-    constructor(address _tokenA, address _tokenB) ERC20("Xyzswap Liquid Pair", "XYZLP") {
+    mapping(address => mapping(address => address)) public getPair;  // get pair address
+
+    constructor() ERC20("Xyzswap Liquid Pair", "XYZLP") {
         owner = msg.sender;
-        tokenA = _tokenA;
-        tokenB = _tokenB;
     }
 
     /******************************************************************************************************
@@ -53,7 +50,26 @@ contract XyzswapV2 is ERC20 {
     *                                                                                                     *
     *******************************************************************************************************/
 
- 
+     function allPairsLength() external view returns (uint) {   // got pair amount
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address) {
+        require(tokenA != tokenB, "Same token!");
+         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        // 判断token0不能为0地址
+        // 此时判断token0就等于同时判断了token0和token1,因为tokenA和tokenB已经进行过大小判断,token0地址是绝对小于任何地址
+        if (token0 == address(0)) revert zeroAddress();
+        // 判断token0是否和token1有产生过交易对,如果没配对那么mapping就是0地址,如果非0地址代表已经配对过了
+        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
+        // 表达式type(x)可用于检索参数x的类型信息(x仅能是合约或整型常量)
+        // type(x).creationCode 获得包含x的合约的bytecode,是bytes类型(不能在合约本身或继承的合约中使用,因为会引起循环引用)
+        bytes memory bytecode = type(xyzswapV2Pair).creationCode;
+        // 将排序好的token对进行打包后通过keccak256得到hash值
+        // 因为两个地址是为确定值,所以salt是可以通过链下计算出来
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        
+    }
     
     /******************************************************************************************************
     *                                                                                                     *
---
+++ contracts/xyzswapV2Pair.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./libraries/SafeMath.sol";
+
+contract xyzswapV2Pair {
+    using SafeMath for uint256;
+}
